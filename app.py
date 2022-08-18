59210ccb710d69fa1781c0b199ad6d8e00a3d70f
integer1998 <111527694+integer1998@users.noreply.github.com>
2022-08-18 13:49:10 -0600

    Update app.py

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ st.set_option('deprecation.showfileUploaderEncoding', False)
 
 @st.cache(allow_output_mutation=True)
 def load_model():
-	model = tf.keras.models.load_model('./model.hdf5')
+	model = tf.keras.models.load_model('./model.h5')
 	return model
 
 
